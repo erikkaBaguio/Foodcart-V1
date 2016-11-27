6941e81e9de58f086605cdb3e3e40d51f7c7d4fc
Kristel Pabillaran <pabillarankristel@ymail.com>
2016-11-28 00:53:19 +0800

    added new_location function in functions.sql

--- api/functions.sql
+++ api/functions.sql
@@ -8,3 +8,21 @@ create or replace function show_restaurant(in par_restoID int, out bigint, out t
 		where Restaurants.id = par_restoID;
 	$$
 	language 'sql';
+
+create or replace function new_location(par_address VARCHAR)
+	returns TEXT as
+	$$
+		DECLARE
+			loc_res TEXT;
+		BEGIN
+			if par_address = '' THEN
+				loc_res = 'Error';
+			ELSE
+				INSERT INTO Location(address) VALUES (par_address);
+
+				loc_res = 'Location Added!'
+			END IF;
+			RETURN loc_res;
+		END;
+	$$
+		language 'plpgsql';
