4028b062f393ed75c3d34449eff67ffb3957edeb
erikkaBaguio <erikka.baguio@gmail.com>
2016-11-28 21:47:05 +0800

    Modified show_restaurant function in functions.sql

--- api/functions.sql
+++ api/functions.sql
@@ -1,28 +1,17 @@
---[GET] Retrieve specific restaurant
+--[GET] Retrieve a specific restaurant
 --select show_restaurant(1);
-create or replace function show_restaurant(in par_restoID int, out bigint, out text, out text, out float, out float, out int, out boolean)
+create or replace function show_restaurant(in par_restoID int, out bigint, out text, out float, out float, out boolean, out text)
 	returns setof record as
 	$$
-		select *
-		from Restaurants
-		where Restaurants.id = par_restoID;
+		select 	Restaurant.*,
+				Location.location_name
+		from Restaurant_location
+		INNER JOIN Restaurant ON (
+			Restaurant_location.restaurant_id = Restaurant.id
+		)
+		INNER JOIN Location ON (
+			Restaurant_location.location_id = Location.id
+		)
+		WHERE Restaurant_location.id = par_restoID;
 	$$
 	language 'sql';
-
-create or replace function new_location(par_address VARCHAR)
-	returns TEXT as
-	$$
-		DECLARE
-			loc_res TEXT;
-		BEGIN
-			if par_address = '' THEN
-				loc_res = 'Error';
-			ELSE
-				INSERT INTO Location(address) VALUES (par_address);
-
-				loc_res = 'Location Added!'
-			END IF;
-			RETURN loc_res;
-		END;
-	$$
-		language 'plpgsql';
