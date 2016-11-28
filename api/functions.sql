8637ec8ba2b1fd87521aecf6db0b8fd89db7f110
erikkaBaguio <erikka.baguio@gmail.com>
2016-11-28 21:49:32 +0800

    Added show_allrestaurants function in functions.sql

--- api/functions.sql
+++ api/functions.sql
@@ -15,3 +15,23 @@ create or replace function show_restaurant(in par_restoID int, out bigint, out t
 		WHERE Restaurant_location.id = par_restoID;
 	$$
 	language 'sql';
+
+
+--[GET] Retrieve all restaurants
+--select show_allrestaurants();
+create or replace function show_allrestaurants(out bigint, out text, out float, out float, out boolean, out text)
+	returns setof record as
+	$$
+		select 	Restaurant.*,
+				Location.location_name
+		from Restaurant_location
+		INNER JOIN Restaurant ON (
+			Restaurant_location.restaurant_id = Restaurant.id
+		)
+		INNER JOIN Location ON (
+			Restaurant_location.location_id = Location.id
+		)
+	$$
+	language 'sql';
+
+
