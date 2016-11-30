c03f32495e33d51d8a0d2d02551ba969bd3c005f
Kristel Pabillaran <pabillarankristel@ymail.com>
2016-11-30 10:23:17 +0800

    added get_user function in views.py

--- app/views.py
+++ app/views.py
@@ -19,14 +19,14 @@ if __name__ == '__main__':
 spcalls = SPcalls()
 
 
-@app.route('/api/foodcart/user/signup/', methods = ['POST'])
+@app.route('/api/foodcart/users/signup/', methods = ['POST'])
 def store_user():
 	data = json.loads(request.data)
 
 	if invalid(jsn['email']):
 		return jsonify({'status': 'error', 'message': 'Error'})
 
-	res = spcalls.spcall("store_user", (
+	user = spcalls.spcall("store_user", (
 			data['fname'],
 			data['mname'],
 			data['lname'],
@@ -39,7 +39,22 @@ def store_user():
 	))
 
 	if 'Error' in res[0][0]:
-		return jsonify({'status': 'error', 'message': res[0][0]})
+		return jsonify({'status': 'error', 'message': user[0][0]})
 
 	else:
-		return jsonify({'status': 'ok', 'message': res[0][0]})
+		return jsonify({'status': 'ok', 'message': user[0][0]})
+
+
+@app.route('/api/foodcart/users/', methods = ['GET'])
+def get_users():
+	user = spcalls.spcall('get_user', ())
+
+	if 'Error' in str(user[0][0]):
+		return jsonify({'status': 'error', 'message': user[0][0]})
+
+	entries = []
+	for r in user:
+		entries.append({'id': str(r[0]), 'fname': str(r[1]), 'mname': str(r[2]), 'lname': str(r[3]), 'address': str(r[4]), 
+						'email': str(r[5]), 'mobile_number': str(r[6]), 'role_id': str(r[8]), 'earned_points': str(r[9])})
+
+	return jsonify({'status': 'ok', 'entries': entries, 'count': len(entries)})
